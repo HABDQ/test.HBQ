a072ba1dc4e7b2244f300f104e298fc35b5d8a67
老六哥 <110911692+HABDQ@users.noreply.github.com>
2022-08-09 21:52:41 +0800

    Update 加法.c

--- 加法.c
+++ 加法.c
@@ -4,9 +4,9 @@ int main()
 	int num1 = 0;
 	int num2 = 0;
 	int sum = 0;
-	scanf("%f%f", &num1, &num2);
+	scanf("%d%d", &num1, &num2);
 	sum = num1 + num2;
-	printf("sum = %f\n",sum);
+	printf("sum = %d\n",sum);
 	return 0;
 	}
 	  
