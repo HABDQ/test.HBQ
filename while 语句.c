b383b22c305e68796f529ee6bc77a46c7c71ec60
老六哥 <110911692+HABDQ@users.noreply.github.com>
2022-08-11 19:14:55 +0800

    Add files via upload

---
+++ while 语句.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	int line = 0;
+	printf("每天学习");
+	while(line<20000)
+	{
+		printf("每天敲代码 %d\n", line);
+		
+	}
+	if(line>=20000)
+		printf("好工作\n");
+	return 0;
+}
