42293bdb3a3d7a4989a95bced44494838e9ee01b
老六哥 <110911692+HABDQ@users.noreply.github.com>
2022-08-12 22:45:57 +0800

    Add files via upload

---
+++ if 和else.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	int a = 0;
+	
+	printf ("你会好好打篮球吗? (1/0):");
+	//printf ("")
+	scanf("%d", &a);
+	if(a == 1)
+	printf("长高10公分\n");
+	else
+	printf("没别人高\n");
+	return 0;
+ } 
